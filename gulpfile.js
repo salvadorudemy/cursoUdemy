1a5529d189c267fd865a654a59a1f8099a8016f6
Salvador <zerattul_8@hotmail.es>
2017-06-21 17:03:40 -0500

    Video02

--- gulpfile.js
+++ gulpfile.js
@@ -27,8 +27,60 @@ const gulp         = require("gulp"),
         nm          : "node_modules",
       },
       files = {
-    
+        CSS : [
+          `${dir.nm}/animate.css/animate.min.css`,
+          `${dir.nm}/font-awesome/css/font-awesome.min.css`,
+          `${dir.nm}/owl.carousel/dist/assets/owl.carousel.min.css`,
+          `${dir.nm}/owl.carousel/dist/assets/owl.theme.default.min.css`,
+          `${dir.nm}/magnific-popup/dist/magnific-popup.css`,
+          "css/main.css",
+        ],
+        mCSS : "main.min.css",
+        JS : [
+          `${dir.nm}/jquery/dist/jquery.min.js`,
+          `${dir.nm}/wowjs/dist/wow.min.js`,
+          `${dir.nm}/jquery-sticky/jquery.sticky.js`,
+          `${dir.nm}/isotope-layout/dist/isotope.pkgd.min.js`,
+          `${dir.nm}/jquery-countto/jquery.countTo.js`,
+          `${dir.nm}/jquery-inview/jquery.inview.min.js`,
+          `${dir.nm}/jquery.countdown/jquery.countdown.js`,
+          `${dir.nm}/magnific-popup/dist/jquery.magnific-popup.min.js`,
+          `${dir.nm}/owl.carousel/dist/owl.carousel.min.js`,
+          "js/main.js"
+        ],
+        mJS : "mian.min.js"
       },
       opts = {
-
+        pug : {
+          pretty : true,
+          locals : {
+            files : files,
+          }
+        }
       };
+//Task Pug
+gulp.task("pug",() => {
+  return gulp.src( `${dis.src}/templates/*.pug` )
+  .pipe(plumber())
+  .pipe(pug(opts.pug))
+  .pipe(gulp.dest(`${dir.development}`))
+  .pipe(browserSync.reload({ stream : true }));
+});
+
+
+// Task server
+// ==========
+
+gulp.task("server", () => {
+  browserSync.init({
+    server : {
+      baseDir           : `${dir.development}`,
+      routes            : {
+        "/node_modules" : `${dir.nm}`
+      }
+    }
+  });
+});
+
+// Tasks
+gulp.task("default", [ "server" ]);
